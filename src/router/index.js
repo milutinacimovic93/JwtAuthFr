218056ecef4a68741231c0ba6512cf04abd8696b
milutinacimovic93 <milutinacimovic93@gmail.com>
2021-06-03 21:16:51 +0200

    Added auth guard and added login component

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import LoginComponent from '../components/LoginComponent'
+import { globalAuthGuard } from '../guards/auth.guard.js'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/about',
+    meta: { authRequired: true }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginComponent
   },
   {
     path: '/about',
     name: 'About',
+    meta: { authRequired: true },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -25,5 +32,7 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach(globalAuthGuard);
 
 export default router
